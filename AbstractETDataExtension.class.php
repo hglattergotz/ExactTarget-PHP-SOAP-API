27e555e62494cda69e8ae4915f2db68d18d4de7c
Henning Glatter-Gotz <henning@glatter-gotz.com>
2012-03-22 15:56:59 -0400

    Add find method to AbstractETDataExtension
    Allow searches of data extensions based on SimpleFilterPart or ComplexFilterPart filters. Returns either an ETCollection or an array.

--- AbstractETDataExtension.class.php
+++ AbstractETDataExtension.class.php
@@ -40,6 +40,110 @@ abstract class AbstractETDataExtension
   }
   
   /**
+   * Find records based on the passed filter.
+   * 
+   * Example usage:
+   * 
+   * $f = new ExactTarget_SimpleFilterPart();
+   * $f->Property = 'id';
+   * $f->Value = $id;
+   * $f->SimpleOperator = ExactTarget_SimpleOperators::equals;
+   *
+   * $de = new MyDataExtension();
+   * $records = $de->find($f, ETCore::HYDRATE_ARRAY);
+   *
+   * @param mixed   $filter        SimpleFilterPart or ComplexFilterPart
+   * @param integer $hydrationMode See ETCore
+   * @return mixed  ETCollection or array, depending on hydration mode
+   * @throws Exception
+   */
+  public function find($filter, $hydrationMode = ETCore::HYDRATE_ARRAY)
+  {
+    try
+    {
+      $typeName = null;
+
+      if ($filter instanceof ExactTarget_SimpleFilterPart)
+      {
+        $typeName = 'SimpleFilterPart';
+      }
+      else if ($filter instanceof ExactTarget_ComplexFilterPart)
+      {
+        $typeName = 'ComplexFilterPart';
+      }
+      else
+      {
+        throw new Exception('First parameter must be of type ExactTarget_SimpleFilterPart or ExactTarget_ComplexFilterPart.');
+      }
+
+      $res = null;
+      
+      $rr = new ExactTarget_RetrieveRequest();
+      $rr->ObjectType = 'DataExtensionObject['.$this->customerKey.']';
+      $rr->Properties = $this->schema;
+      $rr->Filter = ETCore::toSoapVar($filter, $typeName);
+      $rr->Options = null;
+
+      $rrm = new ExactTarget_RetrieveRequestMsg();
+      $rrm->RetrieveRequest = $rr;
+
+      $result = $this->soapClient->Retrieve($rrm);
+
+      $records = array();
+      
+      if (!property_exists($result, 'Results'))
+      {
+        $result->Results = array();
+      }
+      
+      if (!is_array($result->Results))
+      {
+        $result->Results = array($result->Results);
+      }
+      
+      foreach ($result->Results as $fields)
+      {
+        $record = array();
+        
+        foreach ($fields->Properties->Property as $field)
+        {
+          $record[$field->Name] = $field->Value;
+        }
+        
+        if (count($record) > 0)
+        {
+          $records[] = $record;
+        }
+      }
+      
+      if ($hydrationMode == ETCore::HYDRATE_ARRAY)
+      {
+        $res = $records;
+      }
+      else
+      {
+        $coll = new ETCollection();
+        
+        foreach ($records as $record)
+        {
+          $eo = new $this->extensionObjectClassName();
+          $eo->fromArray($record);
+          
+          $coll->add($eo);
+        }
+        
+        $res = $coll;
+      }
+      
+      return $res;
+    }
+    catch (Exception $e)
+    {
+      throw new Exception(__METHOD__ . ':' . __LINE__ . '|' . $e->getMessage());
+    }
+  }
+  
+  /**
    * Get all records from the table (data extension).
    * 
    * @param integer $hydrationMode ETCore::HYDRATE_ARRAY will return an array
